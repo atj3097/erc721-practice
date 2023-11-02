56a137a5eb48da90c9620fb795d6b17a9ad3e24c
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-02 12:54:42 -0500

    updating tests

--- test/NFT.t.sol
+++ test/NFT.t.sol
@@ -2,26 +2,45 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 import "../src/NFT.sol";
 import "../src/Token.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
 
 contract NFTTest is Test {
     NFT public nft;
     Token public token;
+    address public user;
 
     function setUp() public {
+        // Deploy the ERC20 Token contract and mint tokens to the test contract
         token = new Token(100000000);
-        IERC20(token).transfer(msg.sender, 1000);
-        nft = new NFT(2, address(token));
-        IERC20(token).approve(address(nft), 10);
+
+        // Deploy the NFT contract with a mint fee
+        nft = new NFT(2, address(token)); // Assuming the mint fee is 2 tokens
+
+        // Setup a user address
+        user = address(0x1);
+
+        // Give the user some tokens
+        token.transfer(user, 100);
+
+        // Approve the NFT contract to spend tokens on behalf of the user
+        vm.prank(user);
+        token.approve(address(nft), 10);
     }
 
-    function testNFTMint() public {
-        INFT(nft).mintNFTUsingERC20(msg.sender, "idk");
-        assert(IERC721(nft).balanceOf(msg.sender) > 1);
+    function testMintNFT() public {
+        // Check user's token balance before minting the NFT
+        uint256 userTokenBalanceBefore = token.balanceOf(user);
+        assertEq(userTokenBalanceBefore, 100);
+
+        // Mint the NFT using ERC20 tokens
+        vm.prank(user);
+        uint256 tokenId = nft.mintNFTUsingERC20(user, "tokenURI");
+
+        // Check that the NFT was minted and the token balance was deducted
+        uint256 userTokenBalanceAfter = token.balanceOf(user);
+        assertEq(userTokenBalanceAfter, userTokenBalanceBefore - 2);
+        assertEq(nft.balanceOf(user), 1);
+        assertEq(nft.ownerOf(tokenId), user);
     }
 }
